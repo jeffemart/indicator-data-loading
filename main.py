6cffda3456dcadae0e99146974b18228aed94882
jeffemart <jefferson.github@gmail.com>
2023-05-19 16:37:15 -0300

    conexao com banco de dados

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import jwt
+import logging
+from session import conector
+from dotenv import load_dotenv
+
+# Configuração básica do logger
+log_folder = "log"
+os.makedirs(log_folder, exist_ok=True)
+log_file = os.path.join(log_folder, "file.log")
+
+logging.basicConfig(level=logging.DEBUG, filename=log_file, filemode='a',
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+# ...
+connection = conector.Connection()
+con = connection.connect()
