d93a14a3aa6caff2aafcfd3cb8b94d317e1c8793
jeffemart <jefferson.github@gmail.com>
2023-05-23 15:21:10 -0300

    Finalizando chamados

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 import os
+import time
+import datetime
+import schedule
 import logging
 # ...
 from libs import Chamados
@@ -14,5 +17,21 @@ logging.basicConfig(level=logging.DEBUG, filename=log_file, filemode='a',
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 # ...
-chamados = Chamados.Calls()
-chamados.Priority()
+def Routine():
+    hora_atual = datetime.datetime.now().time()
+    hora_inicio = datetime.time(9, 0, 0)
+    hora_fim = datetime.time(17, 0, 0)
+
+    if hora_inicio <= hora_atual <= hora_fim:
+        # Coloque o código que você deseja executar aqui
+        print("Executando o script...")
+    else:
+        print("Fora do horário de execução.")
+
+# Agendar a execução do script a cada minuto
+schedule.every().day.at("09:00").do(Routine)
+
+# Manter o script em execução
+while True:
+    schedule.run_pending()
+    time.sleep(1)
